73726b4a802fcd7314e0286b403722875fd8d021
Shubham Joshi <shubham0009joshi@gmail.com>
2026-08-03 11:33:57 +0530

    Replace student and course queries with customer and order queries

--- EXPERIMENTS/EXPERIMENT_4/4.1.sql
+++ EXPERIMENTS/EXPERIMENT_4/4.1.sql
@@ -1,9 +1,11 @@
-SELECT *
-FROM student
-JOIN course
-ON student.Course_id = course.Course_id;
+SELECT c.customer_name, o.order_date
+FROM customers c
+INNER JOIN orders o ON c.customer_id = o.customer_id;
 
-SELECT *
-FROM student
-LEFT JOIN course
-ON student.Course_id = course.Course_id;
+SELECT c.customer_name, o.product_name
+FROM customers c
+LEFT JOIN orders o ON c.customer_id = o.customer_id;
+
+SELECT p.product_name, o.order_date
+FROM products p
+INNER JOIN orders o ON p.product_name = o.product_name;
